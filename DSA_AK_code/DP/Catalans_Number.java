e66dd0d5501497084db442403c6c8294254158a9
yash kelhe <yashkelhe30@gmail.com>
2024-10-19 01:15:01 +0530

    DP

--- DSA_AK_code/DP/Catalans_Number.java
+++ DSA_AK_code/DP/Catalans_Number.java
@@ -2,7 +2,7 @@ package DP;
 
 import java.util.Arrays;
 
-public class Catalans_nber {
+public class Catalans_Number {
     public static int CalculateCatalansUsingRecursion(int n) {
         if (n == 0 || n == 1) {
             return 1;
@@ -33,6 +33,20 @@ public class Catalans_nber {
 
     }
 
+    public static int usingTabulation(int n) {
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         int n = 4;
         System.out.println("using the recursion : " + CalculateCatalansUsingRecursion(n));
@@ -40,6 +54,9 @@ public class Catalans_nber {
         int dp[] = new int[n + 1];
         Arrays.fill(dp, -1);
         System.out.println("using memorization : " + usingMemorization(n, dp));
+
+        int value = 4;
+        System.out.println("using tabulation method : " + usingTabulation(value));
     }
 
 }
---
+++ DSA_AK_code/DP/Mountain_Ranges.java
@@ -0,0 +1,27 @@
+package DP;
+
+public class Mountain_Ranges {
+
+    // O(n^2)
+    public static int Mountain_Valleys(int n) {
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 0; j < i; j++) {
+                int insideValleys = dp[j];
+                int OutSideValleys = dp[i - j - 1];
+                dp[i] += insideValleys * OutSideValleys;
+            }
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(Mountain_Valleys(n));
+    }
+
+}
